63cedafec705a5031e5b68668f2a4fd8846046eb
muzzu4845-gif <muzzu4845@gmail.com>
2026-04-25 11:26:43 +0530

    fix cors for new vercel url

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,8 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://bus-booking-app-gx5o.vercel.app"
+    "https://bus-booking-app-gx5o.vercel.app",
+    "https://bus-booking-app-roan.vercel.app",
   ],
   credentials: true,
 }));
